75df8555737cce183b831c85c2d9cd6c08712b74
romanb-adr <romanb@adorama.com>
2021-03-19 16:15:41 +0200

    adds test-vm tf resources to test TFC integration

---
+++ test-vm/main.tf
@@ -0,0 +1,65 @@
+provider "google" {
+  region      = var.region
+  project     = var.project
+  credentials = file(var.gcp_auth_file)
+}
+
+
+resource "random_id" "instance_id" {
+  byte_length = 4
+}
+
+
+// reserve External Static IP Address
+resource "google_compute_address" "jenkins-server-ip" {
+  name         = "jenkins-server-ip-1"
+  network_tier = "STANDARD"
+}
+
+
+resource "google_compute_instance" "default" {
+  name                      = "${var.vm_name_prefix}-${random_id.instance_id.hex}"
+  project                   = var.project
+  machine_type              = var.machine_type
+  zone                      = var.zone
+  allow_stopping_for_update = true
+
+
+  boot_disk {
+    initialize_params {
+      image = var.os_image
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+      nat_ip       = google_compute_address.jenkins-server-ip.address
+      network_tier = "STANDARD"
+    }
+  }
+
+  tags = ["non-prod", "ci", "jenkins", "generic"]
+  metadata = {
+    block-project-ssh-keys = false
+  }
+}
+
+
+// Apply the firewall rule to allow external IPs to access this instance
+resource "google_compute_firewall" "jenkins-http" {
+  name    = "default-allow-http-jenkins"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.listen_port]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = var.allowed_networks
+  target_tags   = ["generic"]
+}
+
---
+++ test-vm/variables.tf
@@ -0,0 +1,49 @@
+# IMPORTANT
+# This will be set as ENV variables
+# example: export TF_VAR_var_name
+
+variable "gcp_auth_file" {
+  description = ""
+  type        = string
+}
+
+variable "region" {
+  description = "The region name you want to use for your projects"
+  type        = string
+  default     = "us-east1"
+}
+
+variable "project" {
+  type = string
+}
+
+variable "listen_port" {
+  description = "Port number on which Jenkins will listen"
+  type = string
+}
+
+variable "machine_type" {
+  description = "Machine H/W profile"
+  type = string
+}
+
+variable "zone" {
+  description = "Zone where we run machine"
+  type = string
+}
+
+
+variable "os_image" {
+  description = "OS Image to use for VM spin up"
+  type = string
+}
+
+variable "vm_name_prefix" {
+  description = "VM name prefix; is used for short hostname and/or declaring FQDN; exmple: vm_name_prefix = jenkins-master -> fqdn = jenkins-master.domain.com"
+  type = string
+}
+
+variable "allowed_networks" {
+  description = "List of networks to allow access to HTTP port"
+  type = list
+}
